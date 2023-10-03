c8ea1373b3d95673e96589cae8f2accecdb32f43
zhiyan Yao <rachel2804195717@icloud.com>
2023-10-03 13:49:40 +0200

    help msg deleted from the framework

--- evoman/environment.py
+++ evoman/environment.py
@@ -528,7 +528,6 @@ class Environment(object):
 
             # checks player life status
             if self.player.life == 0:
-                print('player fails\n')
                 ends -= 1
 
                 # tells user that player has lost
@@ -550,7 +549,6 @@ class Environment(object):
 
             # checks enemy life status
             if self.enemy.life == 0:
-                print('player wins\n')
                 ends -= 1
                 if self.visuals:
                     self.screen.fill((250,250,250))
